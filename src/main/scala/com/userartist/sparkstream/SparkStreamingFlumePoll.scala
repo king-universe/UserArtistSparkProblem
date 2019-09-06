04a044576d7b07184f3a368979715d52e3477e21
WangLoveWei <2419465931@qq.com>
2019-09-06 17:29:12 +0800

    新增加flume的poll拉模式和flume的配置

--- src/main/scala/com/userartist/sparkstream/SparkStreamingFlumePoll.scala
+++ src/main/scala/com/userartist/sparkstream/SparkStreamingFlumePoll.scala
@@ -4,9 +4,42 @@ import java.net.InetSocketAddress
 
 import com.userartist.appconf.AppConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import org.apache.spark.streaming.flume.{FlumeUtils, SparkFlumeEvent}
+
+
+//这是flume 的配置文件
+/**
+a1.sources = r1
+a1.sinks = k1
+a1.channels = c1
+
+#source
+a1.sources.r1.channels = c1
+a1.sources.r1.type = spooldir
+#监听该目录下的文件变化
+a1.sources.r1.spoolDir = /opt/data
+a1.sources.r1.fileHeader = true
+
+#channel
+a1.channels.c1.type =memory
+a1.channels.c1.capacity = 20000
+a1.channels.c1.transactionCapacity=5000
+
+#sinks
+a1.sinks.k1.channel = c1
+a1.sinks.k1.type = org.apache.spark.streaming.flume.sink.SparkSink
+a1.sinks.k1.hostname=0.0.0.0
+#a1.sinks.k1.hostname=127.0.0.1
+a1.sinks.k1.port = 9999
+a1.sinks.k1.batchSize= 2000
+  */
+
+
+//启动命令   bin/flume-ng agent -n a1 -c conf -f conf/flume-poll-spark.conf -Dflume.root.logger=info,console
+
+
+
 
 /**
   * 如果是远程主机，一定要配置flume的conf配置文件中的bind为0.0.0.0
